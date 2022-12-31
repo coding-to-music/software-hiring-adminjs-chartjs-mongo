e787d558b55d91baa85e7077af9f99d516c9f86b
coding-to-music <connors.tom@gmail.com>
2022-12-31 04:35:23 +0000

    Build

--- street/street.options.js
+++ street/street.options.js
@@ -7,89 +7,101 @@ const StreetResourceOptions = {
     navigation: hiringMenu,
     properties: {
       _id: {
-        isVisible: {
-          list: false,
-          edit: false,
-          filter: false,
-          show: false,
-        },
-      },
-      name: {
-        isVisible: {
-          isTitle: true,
-          list: true,
-          edit: true,
-          filter: true,
-          show: true,
-        },
-      },
-      fromStreet: {
-        isVisible: {
-          list: true,
-          edit: true,
-          filter: true,
-          show: true,
-        },
-      },
-      to: {
-        isVisible: {
-          list: true,
-          edit: true,
-          filter: true,
-          show: true,
-        },
-      },
-      width: {
-        isVisible: {
-          list: true,
-          edit: true,
-          filter: true,
-          show: true,
-        },
-      },
-      streetLength: {
-        isVisible: {
-          list: true,
-          edit: true,
-          filter: true,
-          show: true,
-        },
-      },
-      area: {
-        isVisible: {
-          list: true,
-          edit: false,
-          filter: true,
-          show: true,
-        },
-      },
-      streetDate: {
-        isVisible: {
-          list: true,
-          edit: true,
-          filter: true,
-          show: true,
-        },
-      },
-      noncity: {
-        isVisible: {
-          list: true,
-          edit: true,
-          filter: true,
-          show: true,
-        },
-      },
-      unnacceptedlength: {
-        isVisible: {
-          list: true,
-          edit: true,
-          filter: true,
-          show: true,
-        },
+        isVisible: false,
       },
     },
   },
 };
+
+// const StreetResourceOptions = {
+//   resource: Street,
+//   options: {
+//     navigation: hiringMenu,
+//     properties: {
+//       _id: {
+//         isVisible: {
+//           list: false,
+//           edit: false,
+//           filter: false,
+//           show: false,
+//         },
+//       },
+//       name: {
+//         isVisible: {
+//           isTitle: true,
+//           list: true,
+//           edit: true,
+//           filter: true,
+//           show: true,
+//         },
+//       },
+//       fromStreet: {
+//         isVisible: {
+//           list: true,
+//           edit: true,
+//           filter: true,
+//           show: true,
+//         },
+//       },
+//       to: {
+//         isVisible: {
+//           list: true,
+//           edit: true,
+//           filter: true,
+//           show: true,
+//         },
+//       },
+//       width: {
+//         isVisible: {
+//           list: true,
+//           edit: true,
+//           filter: true,
+//           show: true,
+//         },
+//       },
+//       streetLength: {
+//         isVisible: {
+//           list: true,
+//           edit: true,
+//           filter: true,
+//           show: true,
+//         },
+//       },
+//       area: {
+//         isVisible: {
+//           list: true,
+//           edit: false,
+//           filter: true,
+//           show: true,
+//         },
+//       },
+//       streetDate: {
+//         isVisible: {
+//           list: true,
+//           edit: true,
+//           filter: true,
+//           show: true,
+//         },
+//       },
+//       noncity: {
+//         isVisible: {
+//           list: true,
+//           edit: true,
+//           filter: true,
+//           show: true,
+//         },
+//       },
+//       unnacceptedlength: {
+//         isVisible: {
+//           list: true,
+//           edit: true,
+//           filter: true,
+//           show: true,
+//         },
+//       },
+//     },
+//   },
+// };
 
 module.exports = {
   StreetResourceOptions,
