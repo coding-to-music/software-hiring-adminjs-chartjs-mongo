0bc655538911f4a159a6d7c7c0c3ffceb9954fc1
coding-to-music <connors.tom@gmail.com>
2023-01-08 01:50:13 +0000

    Build

--- public/custom-components/stats-box.jsx
+++ public/custom-components/stats-box.jsx
@@ -1,37 +1,33 @@
-import { Box, Header, Text, Icon } from "@adminjs/design-system";
+import { Box, Header, Icon, Text } from "@adminjs/design-system";
 
 const StatsBox = ({ label, data, link, icon, bg, color }) => {
-    function onClickToBox() {
-        window.location.href = link;
-    }
+  function onClickToBox() {
+    window.location.href = link;
+  }
 
-    return (
-        <Box
-            color={color}
-            bg={bg}
-            width={[1, 1 / 2, 1 / 3, 1 / 4]} // responsive width
-            padding="1rem"
-            margin="1.5rem" // default margin
+  return (
+    <Box
+      color={color}
+      bg={bg}
+      width={[1, 1 / 2, 1 / 3, 1 / 4]} // responsive width
+      padding="1rem"
+      margin="1.5rem" // default margin
+    >
+      <div onClick={onClickToBox} style={{ cursor: "pointer" }}>
+        <Header.H4 marginTop="1rem" marginBottom="1rem" textAlign="center">
+          {label}
+        </Header.H4>
+        <Text
+          fontSize="2rem"
+          lineHeight="3rem"
+          fontWeight="bold"
+          textAlign="center"
         >
-            <div onClick={onClickToBox} style={{ cursor: "pointer" }}>
-                <Header.H4
-                    marginTop="1rem"
-                    marginBottom="1rem"
-                    textAlign="center"
-                >
-                    {label}
-                </Header.H4>
-                <Text
-                    fontSize="3rem"
-                    lineHeight="3rem"
-                    fontWeight="bold"
-                    textAlign="center"
-                >
-                    {data}
-                </Text>
-            </div>
-        </Box>
-    );
+          {data}
+        </Text>
+      </div>
+    </Box>
+  );
 };
 
 export default StatsBox;
